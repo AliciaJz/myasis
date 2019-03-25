9c21b51cdf77f9bb020a56a8b0a509fa70331083
AliciaJz <aliciajuarez.dev@gmail.com>
2019-03-24 23:56:07 -0600

    lbp logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './simbolo_LBP.jpg';
 import './App.css';
+
+import Amplify from 'aws-amplify';
+import awsmobile from './aws-exports';
+// import { withAuthenticator } from 'aws-amplify-react';
+
+import { API, graphqlOperation } from 'aws-amplify';
+
+Amplify.configure(awsmobile);
+
+const listClases = `query listClases {
+  listClases{
+    items{
+      id
+      nombreClase
+      Ponente
+    }
+  }
+}`
+
+const createAsitente = `mutation CreateAsitente($input: CreateAsitenteInput!) {
+  createAsitente(input: $input) {
+    id
+    NombreGafete
+    NombreCertificado
+    Clases
+  }
+}`
 
 class App extends Component {
   render() {
@@ -17,4 +44,5 @@ class App extends Component {
   }
 }
 
-export default App;
+export default App
+// export default withAuthenticator(App, true);
